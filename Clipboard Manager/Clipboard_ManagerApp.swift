4866956c210a8d1b8c0758195e41cf0d200e2678
Alex Luu <alexluu@berkeley.edu>
2023-02-05 19:04:48 -0800

    Initial Commit

---
+++ Clipboard Manager/Clipboard_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clipboard_ManagerApp.swift
+//  Clipboard Manager
+//
+//  Created by Alex Luu on 2/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Clipboard_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
